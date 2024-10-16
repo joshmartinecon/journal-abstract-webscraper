9d267de72258a9c8ce2f041f415e786492340bf0
joshmartinecon <jmartin2013@gmail.com>
2024-10-16 10:38:25 -0500

    updated 10.16.24

---
+++ science direct abstract webscraper.R
@@ -0,0 +1,95 @@
+rm(list=ls())
+library(RSelenium)
+library(rvest)
+
+# Step 1: Start the RSelenium server
+rD <- rsDriver(browser = "firefox", port = 4545L, verbose = FALSE)
+remDr <- rD$client
+
+# Step 2: Input the URL for the science direct issue you want to read
+initial_url <- "https://www.sciencedirect.com/journal/journal-of-urban-economics/vol/143/"
+remDr$navigate(initial_url)
+
+# Step 3: prove you're not a robot!
+
+# Step 4: Change the working directory for where you want the text file placed
+setwd("C:/Users/jmart/Downloads")
+
+# Step 5: Do you want R to open the links to the articles from this issue in a web browser?
+## Y = Yes, N = No
+open_in_browser <- "Y/N"
+
+##### code ######
+
+# Extract the page source
+page_source <- remDr$getPageSource()[[1]]
+
+# Parse the HTML and extract links
+linkz <- read_html(page_source) %>%
+  html_nodes("a") %>%
+  html_attr("href") %>%
+  .[grepl("/article", .)] %>%
+  .[!grepl(".pdf", .)]
+linkz <- unique(linkz)
+
+# loop web scrape
+y <- list()
+for(i in 2:length(linkz)){
+  remDr$navigate(paste0("https://www.sciencedirect.com", linkz[i]))
+  Sys.sleep(5)
+  page_source <- remDr$getPageSource()[[1]]
+  page_html <- read_html(page_source)
+  
+  title <- page_html %>%
+    html_nodes(".title-text") %>%
+    html_text(trim = TRUE) %>%
+    str_replace_all("\\n", " ") %>%
+    str_replace_all("\\t", " ") %>%
+    str_replace_all(":", " ") %>%
+    str_replace_all("\\.", " ") %>%
+    str_replace_all(",", " ")
+  
+  authors <- page_html %>%
+    html_nodes(".author-group") %>%
+    html_text(trim = TRUE) %>%
+    str_replace_all("\\n", " ") %>%
+    str_replace_all("\\t", " ") %>%
+    str_replace_all(":", " ") %>%
+    str_replace_all("\\.", " ") %>%
+    str_replace_all(",", " ") %>%
+    gsub("Author links open overlay panel", "", .) %>%
+    gsub("[0-9]", "", .) %>%
+    str_squish()
+  
+  abstract <- page_html %>%
+    html_nodes(".abstract.author") %>%
+    html_text(trim = TRUE) %>%
+    str_replace_all("\\n", " ") %>%
+    str_replace_all("\\t", " ") %>%
+    str_replace_all(":", " ") %>%
+    str_replace_all("\\.", " ") %>%
+    str_replace_all(",", " ") %>%
+    str_squish() %>%
+    gsub("Abstract", "", .) %>%
+    .[1]
+  
+  if(length(title) > 0 & length(authors) > 0 & !is.na(abstract)){
+    y[[length(y)+1]] <-  paste(title, authors, abstract)
+  }
+  
+}
+remDr$close()
+rD$server$stop()
+
+# save text
+writeLines(unlist(y), con = paste0(gsub("/", ".", 
+                                        gsub("https://www.sciencedirect.com", "", initial_url)), 
+                                   ".txt"))
+
+# open in browser
+if(open_in_browser == "Y"){
+  for(i in 1:length(linkz)){
+    browseURL(paste0("https://www.sciencedirect.com", linkz[i]))
+    Sys.sleep(3)
+  }
+}
